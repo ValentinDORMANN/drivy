0ae7fb6e9a5ec2fe7fdda1fcea8eed5e7c60712e
ValentinDORMANN <valentin.dormann@gmail.com>
2017-01-13 14:46:12 +0100

    Make rental, car, driver classes and implement calculate rentalPrice

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@
 
 //list of cars
 //useful for ALL exercises
-var cars = [{
+var carsJSON = [{
   'id': 'p306',
   'vehicule': 'peugeot 306',
   'pricePerDay': 20,
@@ -22,7 +22,7 @@ var cars = [{
 //The `price` is updated from exercice 1
 //The `commission` is updated from exercice 3
 //The `options` is useful from exercice 4
-var rentals = [{
+var rentalsJSON = [{
   'id': '1-pb-92',
   'driver': {
     'firstName': 'Paul',
@@ -165,7 +165,91 @@ var rentalModifications = [{
   'pickupDate': '2015-12-05'
 }];
 
-console.log(cars);
-console.log(rentals);
-console.log(actors);
-console.log(rentalModifications);
+/* ======================= CAR ===============================*/
+var Car = function(id, vehicule, pricePerDay, pricePerKm){
+  this.id = id;
+  this.vehicule = vehicule;
+  this.pricePerDay = pricePerDay;
+  this.pricePerKm = pricePerKm;
+}
+Car.prototype.calculateRentalPrice = function(day,km){
+  return this.calculateTimeCost(day)+this.calculateDistanceCost(km);
+};
+Car.prototype.calculateTimeCost = function(day){
+  return this.pricePerDay*day;
+};
+Car.prototype.calculateDistanceCost = function(km){
+  return this.pricePerKm*km;
+};
+
+var CarRepository = function(){
+  this.cars = this.loadDataInJSON(carsJSON);
+};
+CarRepository.prototype.loadDataInJSON = function(data){
+  var cars = [];
+  for(var i = 0; i < data.length; i++){
+    cars.push(new Car(data[i].id, data[i].vehicule, data[i].pricePerDay, data[i].pricePerKm));
+  }
+  return cars;
+};
+CarRepository.prototype.findCarById = function(id){
+  for(var i = 0; i < this.cars.length; i++){
+    if(this.cars[i].id == id){
+      return this.cars[i];
+    }
+  }
+  console.log("Unalble to find this car ("+id+")");
+}
+/* ======================= DRIVER =============================== */
+var Driver = function(firstName, lastName){
+  this.firstName = firstName;
+  this.LastName = lastName;
+}
+/* ======================= RENTAL =============================== */
+var Rental = function(id, driver, carId, pickupDate, returnDate, distance, options, commission){
+  this.id = id;
+  this.driver = driver;
+  this.carId = carId;
+  this.pickupDate = pickupDate;
+  this.returnDate = returnDate;
+  this.distance = distance;
+  this.options = options;
+  this.price = 0;
+  this.commission = commission;
+};
+Rental.prototype.calculateReservedTime = function(){
+  const conversionRateMsToDay = 60*60*1000;
+  var beginDateInMs = this.pickupDate.getTime();
+  var endDateInMs = this.returnDate.getTime();
+  var elapseTime = beginDateInMs - endDateInMs;
+  return Math.ceil(elapseTime%conversionRateMsToDay)+1;
+};
+Rental.prototype.calculateRentalPrice = function(){
+  var carRepository = new CarRepository();
+  // TODO check exception for finfCarBiId
+  this.price = carRepository.findCarById(this.carId).calculateRentalPrice(this.calculateReservedTime(), this.distance);
+  console.log(this.price);
+};
+
+var RentalRepository = function(){
+  this.rentals = this.loadDataInJSON(rentalsJSON);
+  this.calculateRentalPrice();
+};
+RentalRepository.prototype.loadDataInJSON = function(data){
+  var rentals = [];
+  for(var i = 0; i < data.length; i++){
+    rentals.push(new Rental(data[i].id, new Driver(data[i].firstName, data[i].lastName), data[i].carId, new Date(data[i].pickupDate), new Date(data[i].returnDate), data[i].distance, data[i].option, data[i].commission));
+  }
+  return rentals;
+};
+RentalRepository.prototype.calculateRentalPrice = function(){
+  for(var i = 0; i < this.rentals.length; i++){
+    this.rentals[i].calculateRentalPrice();
+  }
+};
+
+// MAIN
+var carRepository = new CarRepository();
+var rentalRepository = new RentalRepository();
+console.log(carRepository);
+console.log(rentalRepository);
